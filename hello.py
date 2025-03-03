54baf90798bcd5ade81229a568c52c9ebef598cf
zavelago <isaiah.lago@marcellin.school.nz>
2025-03-04 10:41:38 +1300

    added code

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+print("My favourite colour is yellow")
+
+name = input("What is your name? ")
+print(f"Hello {name}")
+
+age = input("How old are you? ")
+print(f"{age}! You are younger than me")
