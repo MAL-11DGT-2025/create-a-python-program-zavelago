b611ea68ad346b67f20a3f8b71151c32075fea6f
zavelago <isaiah.lago@marcellin.school.nz>
2025-03-07 13:06:29 +1300

    Learnt new coding

---
+++ lists.py
@@ -0,0 +1,13 @@
+colours = ["red" , "green" , "yellow" , "purple"]
+#print(colours[0])
+#print(colours[1])
+
+#for i in colours
+#   print(1)
+#print(colour[-3])
+
+#print(len(colours))
+
+for i in colours:
+    if i =="yellow":
+        print("There os yellow")
